412507b3a106c7c4dca850d225debe2da70b39c9
wpplng <lina.wappling@gmail.com>
2025-09-04 16:38:06 +0200

    show search results with pagination

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,6 +1,14 @@
-import type { ReactElement } from 'react';
-import { Form, useActionData, useNavigation } from 'react-router';
-import { Box, Button, Typography } from '@mui/material';
+import { useState, type ReactElement } from 'react';
+import { Form, Link, useActionData, useNavigation } from 'react-router';
+import {
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListItemButton,
+  Pagination,
+  Typography,
+} from '@mui/material';
 import type { ICocktail } from '../utilities/types';
 import { Loader } from '../components/UI/Loader';
 
@@ -8,7 +16,13 @@ const SearchPage = (): ReactElement => {
   const results = (useActionData() as ICocktail[]) || [];
   const navigation = useNavigation();
 
-  console.log(results);
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 10;
+
+  const paginated = results.slice(
+    (page - 1) * itemsPerPage,
+    page * itemsPerPage
+  );
 
   return (
     <Box>
@@ -29,6 +43,38 @@ const SearchPage = (): ReactElement => {
       </Form>
 
       {navigation.state === 'submitting' && <Loader />}
+      {results.length > 0 && (
+        <Box mt={3}>
+          <List>
+            {paginated.map((cocktail) => (
+              <ListItem key={cocktail.id} disablePadding>
+                <ListItemButton
+                  component={Link}
+                  to={`/cocktail/${cocktail.id}`}
+                  state={{ fromComponent: true }}
+                >
+                  {cocktail.name}
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+
+          {results.length > itemsPerPage && (
+            <Pagination
+              count={Math.ceil(results.length / itemsPerPage)}
+              page={page}
+              onChange={(_, value) => setPage(value)}
+              sx={{ mt: 2 }}
+            />
+          )}
+        </Box>
+      )}
+
+      {results.length === 0 && navigation.state === 'idle' && (
+        <Typography sx={{ mt: 2 }}>
+          Try searching for a cocktail above
+        </Typography>
+      )}
     </Box>
   );
 };
