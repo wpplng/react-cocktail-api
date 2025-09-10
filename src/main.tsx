512e14f2e77a250e27ebc00d50de904a2d48b8e4
wpplng <lina.wappling@gmail.com>
2025-09-10 19:24:35 +0200

    moved index css to styles folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './router';
-import './index.css';
+import './styles/index.css';
 import { FavoritesProvider } from './context/FavoritesProvider';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme';
