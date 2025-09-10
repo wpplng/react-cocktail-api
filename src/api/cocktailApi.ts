1656a71b17ddf6b21ac857a19a25c26ecd86954a
wpplng <lina.wappling@gmail.com>
2025-09-10 12:06:51 +0200

    moved base url to constants

--- src/api/cocktailApi.ts
+++ src/api/cocktailApi.ts
@@ -1,28 +1,23 @@
 import { mapRawCocktailData } from './mapRawCocktailData';
 import type { ICocktail } from '../utilities/types';
+import { baseUrl } from '../utilities/constants';
 
 export const fetchRandomCocktail = async (): Promise<ICocktail> => {
-  const res = await fetch(
-    'https://www.thecocktaildb.com/api/json/v1/1/random.php'
-  );
+  const res = await fetch(`${baseUrl}random.php`);
   if (!res.ok) throw new Error('Failed to fetch cocktail');
   const data = await res.json();
   return mapRawCocktailData(data.drinks[0]);
 };
 
 export const fetchCocktailById = async (id: string): Promise<ICocktail> => {
-  const res = await fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
-  );
+  const res = await fetch(`${baseUrl}lookup.php?i=${id}`);
   if (!res.ok) throw new Error('Failed to fetch cocktail details');
   const data = await res.json();
   return mapRawCocktailData(data.drinks[0]);
 };
 
 export const searchCocktails = async (query: string): Promise<ICocktail[]> => {
-  const res = await fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`
-  );
+  const res = await fetch(`${baseUrl}search.php?s=${query}`);
   if (!res.ok) throw new Error('Failed to search cocktails');
   const data = await res.json();
   if (!data.drinks) return [];
--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,1 +1,2 @@
+export const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/';
 export const itemsPerPage = 10;
